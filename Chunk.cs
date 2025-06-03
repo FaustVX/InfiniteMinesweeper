a7e2b218415c4abec3102b0c987e237238147dea
FaustVX <jodu035@gmail.com>
2025-06-03 19:38:51 +0200

    Enhance Chunk class with indexer implementation and improve Pos struct formatting

--- Chunk.cs
+++ Chunk.cs
@@ -5,18 +5,33 @@ public class Chunk(Pos pos)
     public const int Size = 8;
     public Pos Pos { get; } = pos;
     public virtual ChunkState State => ChunkState.NotGenerated;
+    public virtual Cell this[Pos pos]
+    {
+        get => default;
+        set { }
+    }
 }
 
 public sealed class ChunkWithMines(Pos pos) : Chunk(pos)
 {
     private readonly Cell[,] _cells = new Cell[Size, Size];
     public override ChunkState State => ChunkState.MineGenerated;
+    public override Cell this[Pos pos]
+    {
+        get => _cells[pos.X, pos.Y];
+        set => _cells[pos.X, pos.Y] = value;
+    }
 }
 
 public sealed class ChunkGenerated(Pos pos) : Chunk(pos)
 {
     private readonly Cell[,] _cells = new Cell[Size, Size];
     public override ChunkState State => ChunkState.FullyGenerated;
+    public override Cell this[Pos pos]
+    {
+        get => _cells[pos.X, pos.Y];
+        set => _cells[pos.X, pos.Y] = value;
+    }
 }
 
 public enum ChunkState
@@ -28,8 +43,25 @@ public enum ChunkState
 
 public readonly record struct Pos(int X, int Y)
 {
-    public static Pos operator +(Pos a, Pos b) => new(a.X + b.X, a.Y + b.Y);
-    public static Pos operator -(Pos a, Pos b) => new(a.X - b.X, a.Y - b.Y);
+    public static Pos operator +(Pos a, Pos b)
+    => new(a.X + b.X, a.Y + b.Y);
+    public static Pos operator -(Pos a, Pos b)
+    => new(a.X - b.X, a.Y - b.Y);
+    public static Pos operator *(Pos a, int scalar)
+    => new(a.X * scalar, a.Y * scalar);
+    public static Pos operator /(Pos a, int scalar)
+    => new(a.X / scalar, a.Y / scalar);
+    public static Pos operator %(Pos a, int scalar)
+    => new(a.X % scalar, a.Y % scalar);
+
+    public Pos ToPosInChunk(out Pos chunkPos)
+    {
+        chunkPos = this / Chunk.Size;
+        return this % Chunk.Size;
+    }
+
+    public Pos ToCellPos(Pos chunkPos)
+    => chunkPos * Chunk.Size + this;
 }
 
 public readonly record struct Cell(int MinesAround, Pos PosInChunk, Pos ChunkPos, bool IsMine, bool IsFlagged, bool IsVisible);
