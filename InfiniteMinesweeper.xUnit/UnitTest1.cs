bb124d373e203055ce80ce60b155681b35e3628a
FaustVX <jodu035@gmail.com>
2025-06-03 20:23:38 +0200

    Add initial xUnit project and unit tests for Pos class

---
+++ InfiniteMinesweeper.xUnit/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿namespace InfiniteMinesweeper.xUnit;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Pos_ToCellPos()
+    {
+        var posInChunk = new Pos(0, 0);
+        var chunkPos = new Pos(0, 0);
+        Assert.Equal(new(0, 0), posInChunk.ToCellPos(chunkPos));
+
+        posInChunk = new(1, 1);
+        chunkPos = new(1, 1);
+        Assert.Equal(new(9, 9), posInChunk.ToCellPos(chunkPos));
+    }
+}
